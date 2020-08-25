320ecc1da3565b24d56cef41f69d15f6c9024251
Amitabh Mehta <amitabh.mehta@gmail.com>
2020-08-26 01:39:21 +0530

    Added scroll lock keypress and auto minimise.

--- Random Activity Gen/test.ps1
+++ Random Activity Gen/test.ps1
@@ -1,17 +1,32 @@
 Clear-Host
 Add-Type -AssemblyName System.Windows.Forms
 $WShell = New-Object -com "Wscript.Shell"
-$PlusOrMinus = 1
+$PlusOrMinus = 10
+
+Add-Type -Name ConsoleUtils -Namespace WPIA -MemberDefinition @'
+   [DllImport("Kernel32.dll")]
+   public static extern IntPtr GetConsoleWindow();
+   [DllImport("user32.dll")]
+   public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+'@
+
+$ConsoleMode = @{ HIDDEN = 0;NORMAL = 1; MINIMIZED = 2; MAXIMIZED = 3; SHOW = 5; RESTORE = 9;}
+$hWnd = [WPIA.ConsoleUtils]::GetConsoleWindow()
+
+$a = [WPIA.ConsoleUtils]::ShowWindow($hWnd, $ConsoleMode.MINIMIZED)
+
+# $a = [WPIA.ConsoleUtils]::ShowWindow($hWnd, $ConsoleMode.HIDDEN)
+# $a = [WPIA.ConsoleUtils]::ShowWindow($hWnd, $ConsoleMode.SHOW)
 
 while ($true)
 {
   $WShell.sendkeys("{SCROLLLOCK}")
   Start-Sleep -Milliseconds 100
   $WShell.sendkeys("{SCROLLLOCK}")
-  Start-Sleep -Seconds 240
+  Start-Sleep -Seconds 40
   $p = [System.Windows.Forms.Cursor]::Position
   $x = $p.X + $PlusOrMinus
   $y = $p.Y + $PlusOrMinus
   [System.Windows.Forms.Cursor]::Position = New-Object System.Drawing.Point($x, $y)
-  $PlusOrMinus *= -10
+  # $PlusOrMinus *= -10
 }
