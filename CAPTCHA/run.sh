ebb64a83b300835a714008e6c9f1084446704d44
Amitabh Mehta <amitabh.mehta@gmail.com>
2020-08-28 14:34:32 +0530

    minor cleanups

--- CAPTCHA/run.sh
+++ CAPTCHA/run.sh
@@ -1,3 +1,3 @@
-cd 	/Users/amit/Documents/code/rewards/cov/
+cd "$(dirname "$0")"
 export PATH="$PATH:/Users/amit/.nvm/versions/node/v12.8.0/bin/"
 /Users/amit/.nvm/versions/node/v12.8.0/bin/npm run start
